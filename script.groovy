6fefe0d525414f42d87cc8e867cfac1785eaca7a
Carmelaa98 <amaged094@gmail.com>
2024-12-22 14:41:26 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'jenkins', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t aeramzy9/java-maven-app:latest .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        # sh 'docker push aeramzy9/java-maven-app:latest'
+        sh 'docker push aeramzy9/java-maven-app:latest'
     }
 } 
 
